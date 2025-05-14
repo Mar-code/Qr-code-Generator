d0133a6001e60ebaad4fcf6d849c0c1c29c734de
marcus <64645089+Mar-code@users.noreply.github.com>
2025-05-14 01:08:20 +0100

    Changes to index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import qr from 'qr-image';
 import fs from 'fs';
 import bodyParser from 'body-parser';
+import env from 'dotenv';
 
-const PORT = 3000;
+env.config();
+const PORT = process.env.PORT|| 3000;
 const app = express();
+let qrCode = '';
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -15,10 +19,21 @@ app.get('/', (req, res)=>{
 
 app.post('/generate', async(req, res) => {
    const url = req.body.url;
-   const qr_img = qr.image(url, { type: 'png' });
-   qr_img.pipe(fs.createWriteStream('public/qr.png'));
-   let qr_path = '/qr.png';
-   res.render('index.ejs', {qrCode: qr_path});
+   const qr_img = qr.imageSync(url, { type: 'png' });
+    qrCode = qr_img;
+   res.render('index.ejs', {qrCode: qr_img.toString('base64')});
+})
+
+app.get('/download', (req, res) => {
+  
+    const filePath = `qr_code.png`;
+    fs.writeFileSync(filePath, qrCode, 'binary');
+    res.download(filePath, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        fs.unlinkSync(filePath); // Delete the file after download
+    });
 })
 
 app.listen(PORT, () => {
